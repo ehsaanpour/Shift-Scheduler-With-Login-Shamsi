73e9b9910c8102707fdb7108aee2374f9662d11f
Ehsan Ehsanpour <46441846+ehsaanpour@users.noreply.github.com>
2025-04-28 18:36:40 +0330

    Add jalali calender to limitation, Fix som bugs

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\EE\Projects\Shift-Scheduler-With-Login\venv"
+set "VIRTUAL_ENV=C:\Users\BEHIN\Git_Folder\Shift-List-With-Authenticator\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
